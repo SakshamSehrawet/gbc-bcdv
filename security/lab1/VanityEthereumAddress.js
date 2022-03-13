e9e7679b962a77cc0efa501f8e78907ea24d0215
Saksham <sakshamsehrawet@gmail.com>
2022-03-13 16:04:29 +0530

    Added comments and fixed function name

--- security/lab1/VanityEthereumAddress.js
+++ security/lab1/VanityEthereumAddress.js
@@ -2,7 +2,8 @@ const randomBytes = require('randombytes');
 const util = require('ethereumjs-util')
 const keccak256 = require('keccak256');
 
-const getValidRand32 = () => {
+// Generates random 32 bytes
+const genValidRand32 = () => {
     let rand32 = randomBytes(32)
     if (util.isValidPrivate(rand32)){
         
@@ -13,18 +14,20 @@ const getValidRand32 = () => {
     }
 }
 
-const genWallet = () => {
-    let pvtKey = getValidRand32()
+// Parse private key to generate address
+const getWallet = () => {
+    let pvtKey = genValidRand32()
     let pubKey = util.privateToPublic(pvtKey)
     let address = '0x' + keccak256(pubKey).slice(12).toString('hex');
     return{pvtKey: '0x' +pvtKey.toString('hex'),pubKey: '0x' +pubKey.toString('hex'),address: address};
 }
 
+// Hit and trial for vanity address
 const getVanityAddress = () => {
     let counter = 0
     while(1){
         counter++
-        let vanityAddress = genWallet()
+        let vanityAddress = getWallet()
         if(vanityAddress.address.slice(2,6) == '1234'){
             console.log(`Tried ${counter} random keys`)
             return(vanityAddress)
